f79fee272fd3b7c30bba1a92b19db9968c136592
wilsoncta <wilsonc.ta1@gmail.com>
2022-03-04 01:58:17 -0800

    A3

--- source/chart_2.R
+++ source/chart_2.R
@@ -7,27 +7,70 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-race <- incarceration %>%
-  select(year, aapi_jail_pop, black_jail_pop, latinx_jail_pop, native_jail_pop, white_jail_pop, total_jail_adm) %>% 
+black_before_pop <- race %>% 
+  filter(year == 1985) %>% 
+  pull(black)
+black_before_pop
+
+black_after_pop <- race %>% 
+  filter(year == 2018) %>% 
+  pull(black)
+black_after_pop
+
+black_comp <- black_after_pop / black_before_pop
+black_comp
+
+white_before_pop <- race %>% 
+  filter(year == 1985) %>% 
+  pull(aapi)
+white_before_pop
+
+white_after_pop <- race %>% 
+  filter(year == 2018) %>% 
+  pull(aapi)
+white_after_pop
+
+white_comp <- white_after_pop / white_before_pop
+white_comp
+
+avg_aapi <- incarceration %>% 
   group_by(year) %>% 
-  na.omit() %>% 
-  mutate(avg_aapi_jail_pop = mean(aapi_jail_pop)) %>% 
-  mutate(avg_black_jail_pop = mean(black_jail_pop)) %>% 
-  mutate(avg_latinx_jail_pop = mean(latinx_jail_pop)) %>% 
-  mutate(avg_native_jail_pop = mean(native_jail_pop)) %>% 
-  mutate(avg_white_jail_pop = mean(white_jail_pop))
+  summarise(aapi = sum(aapi_jail_pop, na.rm = TRUE))
+  
+avg_black <- incarceration %>% 
+  group_by(year) %>% 
+  summarise(black = sum(black_jail_pop, na.rm = TRUE))
+
+avg_latinx <- incarceration %>% 
+  group_by(year) %>% 
+  summarise(latinx = sum(latinx_jail_pop, na.rm = TRUE))
+
+avg_native <- incarceration %>% 
+  group_by(year) %>% 
+  summarise(native = sum(native_jail_pop, na.rm = TRUE))
+
+avg_white <- incarceration %>% 
+  group_by(year) %>% 
+  summarise(white = sum(white_jail_pop, na.rm = TRUE))
+
+race <- avg_aapi %>%
+  left_join(avg_black, by = "year") %>% 
+  left_join(avg_latinx, by = "year") %>% 
+  left_join(avg_native, by = "year") %>% 
+  left_join(avg_white, by = "year") %>% 
+  tail(34)
 View(race)
 
 race_comp <- race %>% 
   ggplot(aes (x = year)) +
-  geom_line(aes(y = race$avg_aapi_jail_pop, color = "AAPI")) +
-  geom_line(aes(y = race$avg_black_jail_pop, color = "Black")) +
-  geom_line(aes(y = race$avg_latinx_jail_pop, color = "Latinx")) +
-  geom_line(aes(y = race$avg_native_jail_pop, color = "Native American")) +
-  geom_line(aes(y = race$avg_white_jail_pop, color = "White")) +
+  geom_line(aes(y = aapi, color = "AAPI")) +
+  geom_line(aes(y = black, color = "Black")) +
+  geom_line(aes(y = latinx, color = "Latinx")) +
+  geom_line(aes(y = native, color = "Native American")) +
+  geom_line(aes(y = white, color = "White")) +
   scale_x_continuous() +
   labs(x = "Year",
        y = "Population",
-       title = "Average Jail Population by Race in the US Over Time",
+       title = "Jail Population by Race in the US Over Time",
        color = "Race")
 race_comp
